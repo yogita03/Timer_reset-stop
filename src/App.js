80a065cf65b8f4e4355457ddea85bfa673b56852
yogita03 <patilyogita408@gmail.com>
2023-06-27 13:34:37 +0530

    timer reset-stop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import './Components/timer.css';
+import React from 'react';
+import { useState , useEffect } from 'react';
 
-function App() {
+ function App() {
+  const [seconds , setSeconds] = useState(0);
+  const [minutes , setMinutes] = useState(0) ;
+   
+ var id;
+  useEffect(()=>{
+    // if(minutes){
+      id = setInterval(()=>{
+      setSeconds(seconds+1);
+
+      if(seconds===59){
+        setMinutes(minutes+1);
+        setSeconds(0);
+      }
+    },1000)
+    return ()=>clearInterval(id);
+    
+  });
+
+  const restart = ()=>{
+    
+    setSeconds(0);
+    setMinutes(0);
+  }
+
+  const stop=()=>{
+    clearInterval(id);
+
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+    <div className='timer'>
+      <div className='container'>
+        <div className='timer_container'>
+  
+      
+    <h1>Timer</h1>
+    <h1>{minutes<10? '0'+minutes:minutes}:{seconds<10? '0'+seconds:seconds}</h1>
+    {/* <button onClick={startHandler}>{`${start ?'stop':'start'}`}</button>   */}
+
+    <button className='restart' onClick={restart}>Restart</button>
+    <button className='stop' onClick={stop}>Stop</button>  
+   
     </div>
-  );
+    </div>
+      </div>
+    // </main>
+  )
 }
 
 export default App;
